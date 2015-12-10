6a6c87568a906516e36c780ccd0e3256857044f1
José Morais <me@jmorais.com>
2015-12-10 18:30:06 -0400

    Dia 10

---
+++ day10/day10-1.rb
@@ -0,0 +1,36 @@
+
+def las(string)
+
+  count = 0
+  result = ""
+  array = []
+
+  0.upto(string.length - 1) do |i|
+    if array[count]
+      if array[count].include? string[i]
+        array[count] += string[i]
+      else
+        count += 1
+        array[count] = string[i]
+      end
+    else
+      array[count] = string[i]
+    end
+  end
+
+  array.each do |sub|
+    result += sub.length.to_s + sub[0]
+  end
+
+  result
+end
+
+input = '1321131112'
+
+1.upto(40) do |i|
+
+  input = las(input)
+
+end
+
+puts input.length
