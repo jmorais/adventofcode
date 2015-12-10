980716e43ffbbaac0b126a80637f79a5da6d2587
José Morais <me@jmorais.com>
2015-12-10 19:29:53 -0400

    Dia 7

---
+++ day7/day7-2.rb
@@ -0,0 +1,44 @@
+$calc = {}
+$hash = {"b" => "16076"}
+
+File.open('day7.in').each do |line|
+  $calc[line.split(' -> ')[1].strip] = line.split(' -> ')[0].split(' ')
+end
+
+def calculate(value)
+
+  begin
+    return Integer(value)
+  rescue
+
+  end
+
+  if !($hash.include?(value))
+    ops = $calc[value]
+
+    if ops.size == 1 # single
+      res = calculate(ops[0])
+    elsif ops.size == 2 # NOT
+      res = ~calculate(ops[1]) & 0xFFFF
+    else
+      case ops[1]
+      when "AND"
+        res = calculate(ops[0]) & calculate(ops[2])
+      when "OR"
+        res = calculate(ops[0]) | calculate(ops[2])
+      when "LSHIFT"
+        res = calculate(ops[0]) << calculate(ops[2])
+      when "RSHIFT"
+        res = calculate(ops[0]) >> calculate(ops[2])
+      end
+    end
+
+    $hash[value] = res
+
+  end
+
+  $hash[value]
+
+end
+
+puts calculate('a')
