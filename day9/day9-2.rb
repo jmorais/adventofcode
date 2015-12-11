9c07f395f471a2f8d8b85af80a0b23b7358eb8bb
José Morais <me@jmorais.com>
2015-12-10 20:36:21 -0400

    Dia 9

---
+++ day9/day9-2.rb
@@ -0,0 +1,30 @@
+
+hash = {}
+total = []
+
+File.open('day9.in').each do |line|
+
+  path, distance = line.split(' = ')
+
+  city_a, city_b = path.split(' to ')
+
+  hash[city_a] ||= {}
+  hash[city_b] ||= {}
+  hash[city_a][city_b] = distance.to_i
+  hash[city_b][city_a] = distance.to_i
+
+end
+
+count = 0
+
+hash.keys.permutation do |path|
+  total[count] = 0
+
+  0.upto(path.size - 2) do |i|
+    total[count] += (hash[path[i]][path[i+1]] || hash[path[i+1]][path[i]])
+  end
+
+  count += 1
+end
+
+puts total.max
